e4af10aade7311e6d52bbebe0a9b9aba702ec537
azizi malek <azizi@retailetics.com>
2022-01-25 17:36:16 +0800

    add hau frame

--- ArduHAU/APM_Config.h
+++ ArduHAU/APM_Config.h
@@ -63,3 +63,5 @@
 //#define USERHOOK_SUPERSLOWLOOP userhook_SuperSlowLoop();  // for code to be run at 1hz
 //#define USERHOOK_AUXSWITCH ENABLED                        // for code to handle user aux switches
 //#define USER_PARAMS_ENABLED ENABLED                       // to enable user parameters
+
+#define FRAME_CONFIG HAU_FRAME		//uncomment to switch to HAU mode
